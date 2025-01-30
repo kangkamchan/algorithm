4b15d57f9dac8da927a5a6614fb474e3ed299f33
kangkamchan <topthanks@naver.com>
2025-01-30 23:56:52 +0900

    [추가] 2775.py

---
+++ DP/2775.py
@@ -0,0 +1,18 @@
+#T = 횟수
+#1<= k = 층 n = 호 <= 14
+
+
+dp = [[0 for _ in range(15)] for _ in range(15)]
+dp[0] = [i for i in range(15)]
+dp[1][1] = 1
+for i in range(1,15):
+  for j in range(1,15):
+    dp[i][j] = dp[i][j-1] + dp[i-1][j]
+
+T = int(input())
+arr = []
+for i in range(T):
+  k, n = map(int,input().split())
+  arr.append(k,n)
+for ele in arr:
+  print(dp[ele[0]][ele[1]])
