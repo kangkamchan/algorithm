76039293c4b91a8f7b63840d71d82ed7c365279a
kangkamchan <topthanks@naver.com>
2025-02-07 19:20:00 +0900

    [수정]14501.py 방식추가

--- DP/14501.py
+++ DP/14501.py
@@ -3,23 +3,32 @@
 #schedule = [] 입력받은 시간, 금액
 import sys
 input = sys.stdin.readline
-def recursion(idx):
-  global m
-  m += 1
-  print(m)
-  if idx == N+1:
-    return 0
-  if idx > N+1:
-    return -99999999
-  if dp[idx] != -1:
-    return dp[idx]
-  dp[idx] = max(recursion(idx+1),recursion(idx+schedule[idx][0])+schedule[idx][1])
-  return dp[idx]
 N = int(input())
-schedule = [0]
+# schedule = [[0,0]]
+# for i in range(N):
+#   a,b = map(int,input().split())
+#   schedule.append([a,b])
+# 탑다운DP
+# def recursion(idx):
+#   if idx == N+1:
+#     return 0
+#   if idx > N+1:
+#     return -99999999
+#   if dp[idx] != -1:
+#     return dp[idx]
+#   dp[idx] = max(recursion(idx+1),recursion(idx+schedule[idx][0])+schedule[idx][1])
+#   return dp[idx]
+# dp = [-1 for _ in range(N+1)]
+# print(recursion(1))
+
+# 바텀업 DP
+schedule = []
 for i in range(N):
   a,b = map(int,input().split())
   schedule.append([a,b])
-dp = [-1 for _ in range(N+1)]
-m = 0
-print(recursion(1))
+dp = [0 for _ in range(N+1)]
+for i in range(N)[::-1]:
+  if schedule[i][0] + i > N:
+    dp[i] = dp[i+1]
+  dp[i] = max(dp[schedule[i][0]+i]+schedule[i][1],dp[i+1])
+print(dp)
