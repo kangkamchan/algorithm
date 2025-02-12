ffb4dc26f0b1c5f4d49e627fb98c54d216dfad2f
kangkamchan <topthanks@naver.com>
2025-02-12 21:34:27 +0900

    [추가]1520.py

---
+++ DP/1520.py
@@ -0,0 +1,18 @@
+#입력 M = 지도의 행 수, N = 지도의 열 수
+#map = M X N 각 칸의 높이
+M, N = map(int,input().split())
+map = [list(map(int,input().split())) for _ in range(M)]
+dp = [[0 for _ in range(N)] for _ in range(M)]
+def recursion(i,j,height):
+  if i < 0 or i >= M or j < 0 or j >= N:
+    return 0
+  if height >= map[i][j]:
+    return 0
+  if i == 0 and j == 0:
+    return 1
+  if dp[i][j] != 0:
+    return dp[i][j]
+  temp_height = map[i][j]
+  dp[i][j] = recursion(i-1,j,temp_height) + recursion(i,j-1,temp_height) + recursion(i+1,j,temp_height) + recursion(i,j+1,temp_height)
+  return dp[i][j]
+print(recursion(M-1,N-1,0))
