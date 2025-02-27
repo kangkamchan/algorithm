ad8f48f3ae77b3e31c84f871ab30619d02905f2a
kangkamchan <topthanks@naver.com>
2025-02-27 23:11:05 +0900

    [추가]9461.py, 11727.py

---
+++ DP/9461.py
@@ -0,0 +1,12 @@
+dp = [0] * 101
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+dp[4] = 2
+for i in range(5,101):
+    dp[i] = dp[i-1] + dp[i-5]
+
+T = int(input())
+for _ in range(T):
+    i = int(input())
+    print(dp[i])
